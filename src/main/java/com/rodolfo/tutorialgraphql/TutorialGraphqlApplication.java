8b2b9bcb88c7b989a2c6dc4b5f9013f94de59060
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2025-05-21 12:37:14 -0300

    init project

---
+++ src/main/java/com/rodolfo/tutorialgraphql/TutorialGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.rodolfo.tutorialgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialGraphqlApplication.class, args);
+    }
+
+}
